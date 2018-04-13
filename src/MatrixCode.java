fc11d8daf56db43f9dbb60f75a43f8c82a283723
Bognar <josip.bognar@optinet.hr>
2018-04-13 10:08:56 +0200

    Matrix code finished

--- src/MatrixCode.java
+++ src/MatrixCode.java
@@ -4,16 +4,17 @@ import java.util.*;
 public class MatrixCode {
 
     public static void main(String[] args) {
-        String Lista[] = {"T", "R", "", "5", "M", "I", "X", "9", ""};
-        Random r = new Random();
-        for (int i = 0; i < 50; i++) {
-            String rnd = Lista[r.nextInt(Lista.length)];
-            System.out.print((rnd) + " ");
-            for (int x = 0; x < 50; x++) {
-                String rnd2 = Lista[r.nextInt(Lista.length)];
-                System.out.print((rnd2) + "\n");
-            }
+        String Lista[] = {"T", "R", "", "5", "S", "I", "M", "9", " ", "C", "o", "B", "e"};
 
+        Random r = new Random();
+
+        for (int t = 0; t <= 100; t++) {
+            for (int i = 0; i < 200; i++) {
+                String rnd = Lista[r.nextInt(Lista.length)];
+                System.out.print((rnd) + " ");
+
+            }
+            System.out.println(" ");
         }
     }
 }
